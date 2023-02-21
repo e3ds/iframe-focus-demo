f9f65947efddb5f64499553011a4fcf93c54a27f
Saeed Sharman <saeed@eagle3dstreaming.com>
2023-02-21 21:41:20 +0600

    format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
+const demos = {
+  soundcloud:
+    '<iframe id="myawesomeiframe001" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/379775672&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>',
+
+  youtube:
+    '<iframe id="myawesomeiframe001" width="420" height="315"src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>'
+};
+
+function Iframe(props) {
+  return (
+    <>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }}
+      />
+    </>
+  );
+}
+
 function App() {
+
+
+  setInterval(() => {
+    document.getElementById('myawesomeiframe001').contentWindow.focus();
+  }, 100);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I frame Demo</h1>
+      <Iframe iframe={demos["soundcloud"]} allow="autoplay" />,
+      <p> the is test text </p>
+      <input />
     </div>
   );
 }
